03040ea7c9fdf2781a2db4e2336a0e29f1a15851
Syed-Hayyan-Raza <sp24bsse0020@maju.edu.pk>
2024-06-06 11:14:47 +0500

    Update minmaxinarray.cpp

--- minmaxinarray.cpp
+++ minmaxinarray.cpp
@@ -3,10 +3,28 @@
 #include<cstring>
 using namespace std;
 
+int findInArray(int arr[5], int n){
+	for(int i=0; i<5; i++){
+		if(arr[i] == n){
+			return 1;
+		}
+		else{
+			return 0;
+		}
+	}
+}
+
 int main(){
 	int arr[5] = {3 , 5, 56 ,5 ,6 };
-	int max = arr[1], min = arr[1];
+	int max = arr[1], min = arr[1], n;
 	
+	cin>>n;
+	
+	if(findInArray(arr , n)){
+		cout<<"Number found"<<endl;
+	}else{
+			cout<<"Number not found in array"<<endl;
+	}
 	
 	
 	for(int i = 0; i<5; i++){
